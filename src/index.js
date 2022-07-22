4a25110cf47943bf99755b0dbde2b6fb76149481
domodfg <domodfg@yahoo.com.hk>
2022-07-23 02:09:09 +0800

    Add an input for task priority

--- src/index.js
+++ src/index.js
@@ -5,15 +5,36 @@ const container = document.querySelector('.container');
 
 const formInput = (() => {
     const title = document.createElement('input');
+    title.setAttribute('required', '');
     const description = document.createElement('input');
-    const priority = document.createElement('input');
+
+    const priority = document.createElement('select');
+    priority.textContent='Priority'
+    const highPrio = document.createElement('option');
+    highPrio.setAttribute('value', 'High');
+    highPrio.textContent='High'
+    const midPrio = document.createElement('option');
+    midPrio.setAttribute('value', 'Mid');
+    midPrio.textContent='Mid'
+    const lowPrio = document.createElement('option');
+    lowPrio.setAttribute('value', 'Low');
+    lowPrio.textContent='Low'
+    priority.appendChild(highPrio);
+    priority.appendChild(midPrio);
+    priority.appendChild(lowPrio);
+
     const dueDate = document.createElement('input');
     dueDate.setAttribute('type', 'date');
+    const submit = document.querySelector('button');
+    const cancel = document.querySelector('button');
     return {
         title,
         description,
         priority,
-        dueDate
+        dueDate,
+        submit,
+        cancel
     }
 })()
+
 
